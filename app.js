ee615ca14f372dc89572a58836fb813b8f12460d
appleearth <appleearth007@gmail.com>
2021-09-23 10:15:08 -0700

    fixed a typo in app.js

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ app.use(
 app.use(express.static(path.join(__dirname, 'public')));
 
 //
-app.user(compression());
+app.use(compression());
 
 //// testing middleware
 app.use((req, res, next) => {
